5a8ff1e5d262508457968100cf6a7155552e9057
Fatma <fceyda@gmail.com>
2020-04-05 22:10:05 -0700

    create chat app

---
+++ wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+const connection = new signalR.HubConnectionBuilder()
+  .withUrl("/chatHub")
+  .build();
+
+
+  //send the message
+  document.getElementById("sendMessage").addEventListener("click", event => {
+    const user = document.getElementById("userName").value;
+    const message = document.getElementById("userMessage").value;
+
+    connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
+    event.preventDefault();
+  });
+
+  //receive the message
+  connection.on("ReceiveMessage", (user, message) => {
+    const recMsg = user + ": " + message;
+    const li = document.createElement("li");
+    li.textContent = recMsg;
+    document.getElementById("messageList").appendChild(li);
+  });
+
+  connection.start().catch(err => console.error(err.toString()));
